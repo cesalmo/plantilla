96e7cd1685afdd7ce1bcaf5165fd12a2bec4f59f
Cesar Alvarez <cesar.alvarez.moran@gmail.com>
2019-12-08 22:33:49 +0100

    plantilla completa

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent", 
+    "sap/ui/core/ComponentSupport"
+    ], 
+    function(UIComponent) {
+	"use strict";
+	return UIComponent.extend("cam.its.Component", {
+		metadata: {
+			manifest: "json"
+		}
+	});
+});
