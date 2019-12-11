edaab6ad06ecb53c9a741b1b6f24d32bd7d461d0
Cesar Alvarez <cesar.alvarez.moran@gmail.com>
2019-12-11 23:05:52 +0100

    implementado navBack

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,22 +1,40 @@
-/* eslint-disable valid-jsdoc */
-
 sap.ui.define(
-    ["sap/ui/core/mvc/Controller"],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller 
-     */
-    function(Controller){
-    
-        return Controller.extend( "cam.its.controller.Detail",{
+  ['sap/ui/core/mvc/Controller', 'sap/ui/core/routing/History'],
+  /**
+   * @class
+   * @extends sap.ui.core.mvc.Controller
+   * @name cam.its.controller.Detail
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   * @param {typeof sap.ui.core.routing.History} History
+   */
 
-            onInit: function(){
-   
-            var oComponent = this.getOwnerComponent();
-            var oRouter = oComponent.getRouter();
-            
-            }
+  function(Controller, History) {
+    return Controller.extend('cam.its.controller.Detail', {
+      /**
+       * @memberof cam.its.controller.Detail
+       */
 
+      onInit: function() {
+        var oComponent = this.getOwnerComponent();
+        var oRouter = oComponent.getRouter();
+        oRouter
+          .getRoute('toDetail')
+          .attachPatternMatched(this._onRouteMatched, this);
+      },
 
-        });
-    }
+      _onRouteMatched: function() {},
+
+      onNavBack: function(e) {
+        var oHistory = History.getInstance();
+        var sPreviousHash = oHistory.getPreviousHash();
+
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1);
+        } else {
+          var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+          oRouter.navTo('toApp', true);
+        }
+      },
+    });
+  },
 );
